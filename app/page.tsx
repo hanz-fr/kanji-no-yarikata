0cbcb831396151f9cfb97d0736012a83804cbc5d
Han <59479453+recta-rrae@users.noreply.github.com>
2024-07-30 10:49:58 +0700

    feat : add filter button and dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
 
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FiSliders } from "react-icons/fi"
+import { DarkModeButton } from "@/components/dark-mode-button";
 
 export default function Home() {
   return (
     <main>
+      <DarkModeButton/>
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col gap-[43px] md:gap-[61px]">
           <h1 className="text-center">漢字のやり方</h1>
-          <Input
-            type="search"
-            placeholder="Search Kanji..."
-            className="focus:ring-0 focus-visible:ring-1 focus-visible:ring-offset-0 border-[#D0D0D0] placeholder:text-[#8D8D8D] transition-all"
-          />
+          <div className="flex">
+            <Input
+              type="search"
+              placeholder="Search kanji..."
+              className="focus:ring-0 focus-visible:ring-1 focus-visible:ring-offset-0 border-[#D0D0D0] placeholder:text-[#8D8D8D] transition-all rounded-tr-none rounded-br-none z-10"
+            />
+            <Button variant="outline" className="rounded-tl-none border-[#D0D0D0] border-l-0 rounded-bl-none">
+              <FiSliders />
+            </Button>
+            <div></div>
+          </div>
         </div>
       </div>
     </main>
