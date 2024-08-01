bcd68ef1d568d8118b8458caad50c61519963b96
Han <59479453+recta-rrae@users.noreply.github.com>
2024-08-01 11:43:39 +0700

    refactor : modify filter dropdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import { Input } from "@/components/ui/input";
 
 import { DarkModeButton } from "@/components/dark-mode-button";
@@ -19,9 +17,6 @@ export default function Home() {
               className="focus:ring-0 focus-visible:ring-1 focus-visible:ring-offset-0 border-[#D0D0D0] placeholder:text-[#8D8D8D] transition-all rounded-tr-none rounded-br-none z-10"
             />
             <FilterDropdown/>
-            {/* <Button variant="outline" className="rounded-tl-none border-[#D0D0D0] border-l-0 rounded-bl-none">
-              <FiSliders />
-            </Button> */}
             <div></div>
           </div>
         </div>
--- components/filter-dropdown.tsx
+++ components/filter-dropdown.tsx
@@ -9,21 +9,80 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { FiSliders } from "react-icons/fi"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FiSliders } from "react-icons/fi";
 
 const FilterDropdown = () => {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="border rounded-md rounded-tl-none border-[#D0D0D0] dark:border-zinc-800 border-l-0 rounded-bl-none px-3">
+      <DropdownMenuTrigger className="border rounded-md rounded-tl-none border-[#D0D0D0] dark:border-zinc-800 border-l-0 rounded-bl-none px-3 hover:bg-gray-100 dark:hover:bg-zinc-900 transition-colors">
         <FiSliders />
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>Something</DropdownMenuLabel>
+      <DropdownMenuContent className="">
+        <DropdownMenuLabel>Filters</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Item 1</DropdownMenuItem>
-        <DropdownMenuItem>Item 2</DropdownMenuItem>
-        <DropdownMenuItem>Item 3</DropdownMenuItem>
-        <DropdownMenuItem>Item 4</DropdownMenuItem>
+        <DropdownMenuItem>
+          <div className="w-full flex gap-2 justify-between">
+            <span className="my-auto font-light">JLPT</span>
+            <Select>
+              <SelectTrigger className="w-20">
+                <SelectValue placeholder="..." />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All</SelectItem>
+                <SelectItem value="N5">N5</SelectItem>
+                <SelectItem value="N4">N4</SelectItem>
+                <SelectItem value="N3">N3</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <div className="w-full flex gap-2 justify-between">
+            <span className="my-auto font-light">Grade</span>
+            <Select>
+              <SelectTrigger className="w-20">
+                <SelectValue placeholder="..." />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All</SelectItem>
+                <SelectItem value="1">1</SelectItem>
+                <SelectItem value="2">2</SelectItem>
+                <SelectItem value="3">3</SelectItem>
+                <SelectItem value="4">4</SelectItem>
+                <SelectItem value="5">5</SelectItem>
+                <SelectItem value="6">6</SelectItem>
+                <SelectItem value="7">7</SelectItem>
+                <SelectItem value="8">8</SelectItem>
+                <SelectItem value="9">9</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <div className="w-full flex gap-2 justify-between">
+            <span className="my-auto font-light">Search by</span>
+            <Select>
+              <SelectTrigger className="w-20">
+                <SelectValue placeholder="..." />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All</SelectItem>
+                <SelectItem value="N5">Kanji</SelectItem>
+                <SelectItem value="N4">Meaning</SelectItem>
+                <SelectItem value="N3">On'yomi</SelectItem>
+                <SelectItem value="N3">Kun'yomi</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
