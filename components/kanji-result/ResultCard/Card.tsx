de7ec105432f18fc8cf014390d1284997393e136
Han <59479453+recta-rrae@users.noreply.github.com>
2024-08-08 23:51:26 +0700

    refactor : ResultCard refactoring complete

--- components/kanji-result/ResultCard/Card.tsx
+++ components/kanji-result/ResultCard/Card.tsx
@@ -1,9 +1,12 @@
-'use client';
-
 import React from "react";
-
-import { useRouter } from "next/navigation";
 import { IResultCard } from "@/interfaces";
+import {
+  Container,
+  Kanji,
+  KanjiMeaningText,
+  KanjiGroupingContainer,
+  OnKun,
+} from "./CardComponents";
 
 const ResultCard: React.FC<IResultCard> = ({
   id,
@@ -12,48 +15,21 @@ const ResultCard: React.FC<IResultCard> = ({
   onyomiRomaji,
   kunyomi,
   kunyomiRomaji,
-  meaning
+  meaning,
 }) => {
-  
-  const router = useRouter();
-
   return (
-    <div
-      className="border border-r-0 border-l-0 border-b-0 border-t-gray-200 dark:border-t-zinc-800 border-b-gray-200 w-full h-fit hover:bg-gray-100 dark:hover:bg-zinc-800 hover:cursor-pointer transition-colors"
-      onClick={() => router.push(`/kanji/${id}`)}
-    >
-      <div className="flex justify-between h-full gap-4 md:gap-8">
-        <div className="flex gap-2 md:gap-4">
-          {/* Kanji Leading */}
-          <span className="w-10 md:w-12 h-10 md:h-12 my-auto">
-            <p className="font-bold text-2xl text-center p-2">{kanji}</p>
-          </span>
-          {/* On'yomi Kun'yomi Column */}
-          <div className="flex gap-2 md:gap-4">
-            <div className="flex flex-col gap-5 my-auto">
-              <div className="px-1 text-nowrap text-xs text-center text-[#C2C2C2] border">
-                音読み
-              </div>
-              <div className="px-1 text-xs text-center text-[#C2C2C2] border">
-                訓読み
-              </div>
-            </div>
-            <div className="flex flex-col gap-2 my-2">
-              <div className="flex flex-col">
-                <span className="onyomi-text">{onyomi}</span>
-                <span className="onyomi-romaji-text">{onyomiRomaji}</span>
-              </div>
-              <div className="flex flex-col">
-                <div className="kunyomi-text">{kunyomi}</div>
-                <div className="kunyomi-romaji-text">{kunyomiRomaji}</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* Meaning */}
-        <div className="my-auto text-[14px] truncate">({meaning})</div>
-      </div>
-    </div>
+    <Container kanjiId={id}>
+      <KanjiGroupingContainer>
+        <Kanji kanji={kanji} />
+        <OnKun
+          onyomi={onyomi}
+          onyomiRomaji={onyomiRomaji}
+          kunyomi={kunyomi}
+          kunyomiRomaji={kunyomiRomaji}
+        />
+      </KanjiGroupingContainer>
+      <KanjiMeaningText meaning={meaning} />
+    </Container>
   );
 };
 
---
+++ components/kanji-result/ResultCard/CardComponents.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export const Container = (props: {
+  children: React.ReactNode;
+  kanjiId: string;
+}) => {
+  const router = useRouter();
+
+  return (
+    <div
+      className="border border-r-0 border-l-0 border-b-0 border-t-gray-200 dark:border-t-zinc-800 border-b-gray-200 w-full h-fit hover:bg-gray-100 dark:hover:bg-zinc-800 hover:cursor-pointer transition-colors"
+      onClick={() => router.push(`/kanji/${props.kanjiId}`)}
+    >
+      <div className="flex justify-between h-full gap-4 md:gap-8">
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export const KanjiGroupingContainer = (props: {
+  children: React.ReactNode;
+}) => {
+  return(
+    <div className="flex gap-2 md:gap-4">{props.children}</div>
+  )
+};
+
+export const Kanji = (props: { kanji: string }) => {
+  return (
+    <span className="w-10 md:w-12 h-10 md:h-12 my-auto">
+      <p className="font-bold text-2xl text-center p-2">{props.kanji}</p>
+    </span>
+  );
+};
+
+export const OnKun = (props: {
+  onyomi: string;
+  onyomiRomaji: string;
+  kunyomi: string;
+  kunyomiRomaji: string;
+}) => {
+  return (
+    <div className="flex gap-2 md:gap-4">
+      <div className="flex flex-col gap-5 my-auto">
+        <div className="px-1 text-nowrap text-xs text-center text-[#C2C2C2] border">
+          音読み
+        </div>
+        <div className="px-1 text-xs text-center text-[#C2C2C2] border">
+          訓読み
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 my-2">
+        <div className="flex flex-col">
+          <span className="onyomi-text">{props.onyomi}</span>
+          <span className="onyomi-romaji-text">{props.onyomiRomaji}</span>
+        </div>
+        <div className="flex flex-col">
+          <div className="kunyomi-text">{props.kunyomi}</div>
+          <div className="kunyomi-romaji-text">{props.kunyomiRomaji}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const KanjiMeaningText = (props: { meaning: string }) => {
+  return <div className="my-auto text-[14px] truncate">({props.meaning})</div>;
+};
