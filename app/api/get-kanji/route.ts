77d9e75e13652557ab4038f84e4e50d022f6aaf7
hanz-fr <raihanf787@gmail.com>
2024-09-03 16:57:07 +0700

    feat : navigate to selected kanji by id page complete

--- app/api/get-kanji/route.ts
+++ app/api/get-kanji/route.ts
@@ -2,7 +2,10 @@ import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
-  const id = request.nextUrl.searchParams;
+  const searchParams = request.nextUrl.searchParams;
+  const id = searchParams.get('id');
+
+  console.log(id);
 
   let data = await fetch(`${API_URL}/kanji/${id}`);
   let res = await data.json();
--- app/kanji/[id]/page.tsx
+++ app/kanji/[id]/page.tsx
@@ -1,17 +1,34 @@
 "use client";
-import React, { useState } from "react";
+
+import React, { useEffect, useState } from "react";
 
 import { FiArrowLeft, FiVolume2 } from "react-icons/fi";
 import { MdOutlineDraw } from "react-icons/md";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import Loading from "./loading";
+import { IKanji } from "@/interfaces";
 
-const KanjiPage = () => {
+const KanjiPage = ({ params }: { params: { id: string } }) => {
+  const id = params.id;
+
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [kanjiData, setKanjiData] = useState<IKanji>();
+  const [errorStatus, setErrorStatus] = useState(null);
 
-  return isLoading ? <Loading/> : (
+  useEffect(() => {
+    fetch(`/api/get-kanji?id=${id}`).then((res) =>
+      res.json().then((data) => {
+        setKanjiData(data?.data);
+        setIsLoading(false);
+      })
+    );
+  }, []);
+
+  return isLoading ? (
+    <Loading />
+  ) : (
     <div className="flex flex-col gap-[10px] px-5 py-3">
       <div className="w-full md:w-1/2 md:mx-auto">
         <FiArrowLeft
@@ -26,8 +43,10 @@ const KanjiPage = () => {
       {/* Kanji Heading Container */}
       <div className="flex justify-center w-full">
         <div className="flex flex-col gap-3">
-          <span className="flex justify-center text-[60px]">光</span>
-          <span className="flex justify-center">(ray, light)</span>
+          <span className="flex justify-center text-[60px]">
+            {kanjiData?.kanji}
+          </span>
+          <span className="flex justify-center">({kanjiData?.meaning})</span>
           <div className="flex justify-center gap-2">
             <Button className="p-3 rounded-full bg-[#D9D9D9] ">
               <FiVolume2 className="text-lg" color="black" />
@@ -41,17 +60,19 @@ const KanjiPage = () => {
       {/* JLPT, Grade, and Strokes Container */}
       <div className="flex gap-1 justify-evenly w-full md:w-1/2 md:mx-auto h-1/2 rounded-sm border py-4">
         <div className="w-14 h-full flex flex-col text-center rounded-md my-auto">
-          <span className="font-normal text-[15px]">N3</span>
+          <span className="font-normal text-[15px]">{kanjiData?.jlpt}</span>
           <span className="font-thin text-[12px]">JLPT</span>
         </div>
         <div className="vertical-separator"></div>
         <div className="w-14 h-full flex flex-col text-center rounded-md my-auto">
-          <span className="font-normal text-[15px]">2</span>
+          <span className="font-normal text-[15px]">{kanjiData?.grade}</span>
           <span className="font-thin text-[12px]">Grade</span>
         </div>
         <div className="vertical-separator"></div>
         <div className="w-14 h-full flex flex-col text-center rounded-md my-auto">
-          <span className="font-normal text-[15px]">6</span>
+          <span className="font-normal text-[15px]">
+            {kanjiData?.strokeCount}
+          </span>
           <span className="font-thin text-[12px]">Stroke(s)</span>
         </div>
       </div>
@@ -67,8 +88,10 @@ const KanjiPage = () => {
             </span>
           </div>
           <div className="flex flex-col">
-            <span className="text-sm">コウ</span>
-            <span className="text-sm text-[#9E9E9E] font-thin">kou</span>
+            <span className="text-sm">{kanjiData?.onyomi}</span>
+            <span className="text-sm text-[#9E9E9E] font-thin">
+              {kanjiData?.onyomiRomaji}
+            </span>
           </div>
         </div>
         <div className="flex justify-start gap-7">
@@ -81,9 +104,9 @@ const KanjiPage = () => {
             </span>
           </div>
           <div className="flex flex-col">
-            <span className="text-sm">ひか(る)、ひかり</span>
+            <span className="text-sm">{kanjiData?.kunyomi}</span>
             <span className="text-sm text-[#9E9E9E] font-thin">
-              hikaru, hikari
+              {kanjiData?.kunyomiRomaji}
             </span>
           </div>
         </div>
@@ -92,50 +115,41 @@ const KanjiPage = () => {
       <div className="flex flex-col w-full px-5 gap-4 md:w-1/2 md:mx-auto h-1/2 rounded-sm border py-4 text-sm">
         <div className="flex w-full justify-between">
           <span className="font-normal text-nowrap">Radical :</span>
-          <span className="font-thin text-end">儿 (legs)</span>
-        </div>
-        <div className="flex w-full justify-between">
-          <span className="font-normal text-nowrap">Frequency :</span>
-          <span className="font-thin text-end">Common</span>
-        </div>
-        <div className="flex w-full justify-between">
-          <span className="font-normal text-nowrap">Components :</span>
           <span className="font-thin text-end">
-            ⺌ (小) small
-            <br />兀 one pair of legs
+            {kanjiData?.radical} ({kanjiData?.radicalNumber})
           </span>
         </div>
         <div className="flex w-full justify-between">
+          <span className="font-normal text-nowrap">Frequency :</span>
+          <span className="font-thin text-end">{kanjiData?.frequency}</span>
+        </div>
+        <div className="flex w-full justify-between">
+          <span className="font-normal text-nowrap">Components :</span>
+          <span className="font-thin text-end">{kanjiData?.components}</span>
+        </div>
+        <div className="flex w-full justify-between">
           <span className="font-normal text-nowrap">Joyo Status :</span>
-          <span className="font-thin text-end">常用 jōyō (regular use)</span>
+          <span className="font-thin text-end">
+            {kanjiData?.joyoListStatus}
+          </span>
         </div>
         <hr />
         <div className="flex flex-col w-full justify-between">
           <span>Mnemonic :</span>
-          <span className="font-thin">
-            A person carries fire that shines light
-          </span>
+          <span className="font-thin">{kanjiData?.mnemonic}</span>
         </div>
       </div>
       {/* Example Container */}
       <div className="flex flex-col w-full px-5 gap-4 md:w-1/2 md:mx-auto h-1/2 rounded-sm border py-4 text-sm">
         <span>Examples</span>
         <table className="table-fixed w-full">
-          <tr className="text-start md:text-center">
-            <td className="p-2 font-semibold">日光</td>
-            <td className="p-2">にっこう</td>
-            <td className="p-2">sunshine, sunlight</td>
-          </tr>
-          <tr className="text-start md:text-center">
-            <td className="p-2 font-semibold">月光</td>
-            <td className="p-2">げっこう</td>
-            <td className="p-2">moonlight</td>
-          </tr>
-          <tr className="text-start md:text-center">
-            <td className="p-2 font-semibold">光年</td>
-            <td className="p-2">こうねん</td>
-            <td className="p-2">light year</td>
-          </tr>
+          {kanjiData?.examples?.map((e) => (
+            <tr className="text-start md:text-center">
+              <td className="p-2 font-semibold"></td>
+              <td className="p-2"></td>
+              <td className="p-2"></td>
+            </tr>
+          ))}
         </table>
       </div>
     </div>
